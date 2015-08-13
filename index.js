5ab8f0664656ca5f99861dc5b0bdba22a720c703
JYNY <simon54894@gmail.com>
2015-08-13 16:26:01 +0800

    tab to space

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ var app = express();
 app.use('/', express.static('public'));
 
 app.get('/near_sites/', function (req, res){
-	//res.send('Hello World!');
-	//console.log(req.query.addr);
-	geo(req.query.addr, function(mylocation, near_sites){
-		var reply = {
-			location: mylocation, 
-			near_sites: near_sites
-		};
-		res.setHeader('Content-Type', 'application/json');
-		res.send(reply);
-	});
+    //res.send('Hello World!');
+    //console.log(req.query.addr);
+    geo(req.query.addr, function(mylocation, near_sites){
+        var reply = {
+            location: mylocation, 
+            near_sites: near_sites
+        };
+        res.setHeader('Content-Type', 'application/json');
+        res.send(reply);
+    });
 });
 
 var server = app.listen(8080);
